835b347c0fe617e5126a59d7566bfe172d7854c0
Julie Theriault <joules2319@gmail.com>
2019-11-27 14:16:13 -0700

    corrected special characters to reference correct variable

--- script.js
+++ script.js
@@ -23,17 +23,19 @@ var lowerCase = confirm("Should your password contain lower case alphabetic char
 var numbers = confirm("Should your password contain numbers?");
 
 // set password with loop
-while (length !== password.length) {
+for (i = length; i > 0; i--) {
 	var charType = Math.floor(Math.random() * 4);
 
 	if (charType === 0 && specChars) {
-		passwordTemp = password.concat(specChars[Math.floor(Math.random() * specChars.length)]);
+		passwordTemp = password.concat(specials[Math.floor(Math.random() * specials.length)]);
 	} else if (charType === 1 && upperCase) {
 		passwordTemp = password.concat(alpha[Math.floor(Math.random() * alpha.length)].toUpperCase());
 	} else if (charType === 2 && lowerCase) {
 		passwordTemp = password.concat(alpha[Math.floor(Math.random() * alpha.length)]);
 	} else if (charType === 3 && numbers) {
 		passwordTemp = password.concat(nums[Math.floor(Math.random() * nums.length)]);
+	} else {
+		i++;
 	}
 	password = passwordTemp
 	console.log(password);
