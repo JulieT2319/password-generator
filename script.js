22e3c90e2f8ab3d60b1181c4ceacc54b6c35452c
Julie Theriault <joules2319@gmail.com>
2019-11-27 20:52:01 -0700

    added copyText function

--- script.js
+++ script.js
@@ -54,3 +54,11 @@ function generatePassword() {
 		document.getElementById("password").innerHTML = password;
 	}
 }
+
+function copyText() {
+	var copyText = document.getElementById("password");
+	copyText.select();
+	copyText.setSelectionRange(0, 99999)
+	document.execCommand("copy");
+	alert("Copied the text: " + copyText.value);
+}
